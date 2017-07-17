a1fca526356eb527fa707c4bcfaae6a4f6debb2a
Adrian Pearman <adrianpearman12@gmail.com>
2017-07-17 19:15:17 -0400

    first test complete

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,8 +2,19 @@ module StringCalculator
 
   def self.add(string)
     # code to be tested goes here...
+    # return string.to_i * 0
+    if string != nil
+      return 0
+    end
+
+
+
   end
 
   # ...and here when the above becomes too complex.
 
 end
+
+
+# TDD - Test Driven Development
+# BDD - Behaviour Driven Development
